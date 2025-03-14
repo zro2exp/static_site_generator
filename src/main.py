19eb52c1b0749d0455d4daa794c3a6d94e9dfca9
zro2exp <zro2exp@proton.me>
2025-03-13 19:52:46 -0600

    add textnode.py

---
+++ src/main.py
@@ -0,0 +1,9 @@
+
+from textnode import TextNode, TextType
+
+def main():
+    text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.zro2xp.me")
+
+    print(text_node)
+
+main()
