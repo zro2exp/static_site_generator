b8fc1d6978199abc515a899bb0eb557010247c27
zro2exp <zro2exp@proton.me>
2025-04-03 11:55:02 -0600

    add parentnode and some tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -33,3 +33,21 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("Invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
+
--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 
 from textnode import TextNode, TextType
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 def main():
     text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.zro2xp.me")
-    node = LeafNode("a", "Click me!", {"href": "https://www.google.com"}).to_html()
+    node = ParentNode("p",
+    [
+        LeafNode("b", "Bold text"),
+        LeafNode(None, "Normal text"),
+        LeafNode("i", "italic text"),
+        LeafNode(None, "Normal text"),
+    ]).to_html()
 
     print(text_node)
     print(node)
